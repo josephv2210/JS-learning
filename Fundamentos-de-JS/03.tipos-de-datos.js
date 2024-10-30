a8177dcc0eda0721d1838c50a88931ae5b7525b7
Joseph Vallejo <joseph.2210@hotmail.es>
2024-10-30 17:52:28 -0500

    data types

---
+++ Fundamentos-de-JS/03.tipos-de-datos.js
@@ -0,0 +1,41 @@
+//PRIMITIVOS
+
+//string
+let nombre = 'Juan'
+
+//number
+
+let edad = 25
+
+//boolen
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol('único')
+
+// bigint
+let numeroGrande = 2n
+
+//COMPLEJOS
+
+
+//object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S',
+    año: 200
+}
+
+//array
+let frutas = ['manzana', 'banano', 'uvas']
+
+//function
+function saludar () {
+    
+}
